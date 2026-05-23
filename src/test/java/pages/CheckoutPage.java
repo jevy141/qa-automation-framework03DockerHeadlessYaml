b45e67c3bb8e7e39b2c33c8a9418b5383a05e801
Sohan Rana <sohan_rana@ymail.com>
2026-05-23 13:37:42 +0530

    In complete checkout method aslo added action import

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
