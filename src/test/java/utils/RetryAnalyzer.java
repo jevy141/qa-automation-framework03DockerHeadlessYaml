cc6b2f0e5020dc4846df3df0845eae48fc325462
Sohan Rana <sohan_rana@ymail.com>
2026-05-26 15:43:59 +0530

    Change retry method to count 1

--- src/test/java/utils/RetryAnalyzer.java
+++ src/test/java/utils/RetryAnalyzer.java
@@ -6,10 +6,13 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     int count = 0;
-    int maxTry = 2;
+    int maxTry = 1;
 
     @Override
     public boolean retry(ITestResult result) {
+    	if (result.getStatus() == ITestResult.SUCCESS) {
+    		return false;
+    	}
 
         if (count < maxTry) {
             count++;
